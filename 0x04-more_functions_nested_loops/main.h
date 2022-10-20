2c7bb6febfe9133dc4bdb3135d2529227a1699e9
Lindarose247 <ndlovulindarose@gmail.com>
2022-10-20 05:49:41 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,42 @@
+#ifndef MAIN_H
+
+#define Main_H
+
+/**
+ * File -main.h
+ * Author: Linda Ndlovu
+ * Descrp: This header contains the prototypes for all functions
+ * 	used forthe project 0x04-more_functions_nested_loops.
+ */
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+
+
+
+
+
+
+
+
+
+
+#endif
