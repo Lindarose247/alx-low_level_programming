b3ef16213ef74da79e6b0190a3476367899a9e90
Lindarose247 <ndlovulindarose@gmail.com>
2022-10-24 12:29:15 -0700

    Task 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -21,11 +21,11 @@ void print_array(int *a, int n)
 
 		if (i != (n - 1))
 
-			printf("%d, ", a[i]);
+			_putchar("%d, ", a[i]);
 
 		else
 
-			printf("%d", a[i]);
+			_putchar("%d", a[i]);
 
 	}
 
