b2bf2082e59e8734c50cc3b165fb1d48568ba571
Lindarose247 <ndlovulindarose@gmail.com>
2022-10-27 13:25:55 +0200

    Functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
-
 void print_alphabet_x10(void)
 {
-	int ten;
+	char ch;
+	int i;
 
-	char letter;
+	i = 0;
 
-	for (ten = 0; ten <= 9; ten++)
+	while (i < 10)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-
-			_putchar(letter);
-
-				_putchar('\n');
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
 	}
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
-
 /**
- * print_last_digit - Prints the last digit of a number.
- * @n: The number in question.
- * Return: Value of the last digit
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
-
 int print_last_digit(int n)
-
 {
-	int last_digit = n % 10;
+	int a;
 
-	if (last_digit < 0)
+	if (n < 0)
+		n = -n;
 
-		last_digit *= -1;
+	a = n % 10;
 
-	_putchar(last_digit + '0');
+	if (a < 0)
+		a = -a;
 
-	return (last_digit);
+	_putchar(a + '0');
+
+	return (a);
 }
