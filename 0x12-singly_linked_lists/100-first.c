bba26be0434de50493d371029a41e56c76ad04de
Lindarose247 <ndlovulindarose@gmail.com>
2022-11-25 02:18:32 +0200

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/**
-* premain - Prints before main prints
-* Return: void
-*/
+void __attribute__((constructor)) hare(void);
 
-void first(void)
+/**
+ * hare - Prints a string before the
+ *        main function is executed.
+ */
+void hare(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
